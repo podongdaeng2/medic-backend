69d437cc484b554ab8befe6d0c477c08b8131a47
SF93 <kdman98@naver.com>
2024-06-08 21:33:11 +0900

    fix for android temp

--- podong-backend/src/main/kotlin/podongdaeng2/chatgpt/SimpleService.kt
+++ podong-backend/src/main/kotlin/podongdaeng2/chatgpt/SimpleService.kt
@@ -3,6 +3,7 @@ package podongdaeng2.chatgpt
 import exposed.model.FoodInfo
 import exposed.model.FoodIntake
 import exposed.model.MealTimeType
+import java.lang.NumberFormatException
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
@@ -48,49 +49,88 @@ object SimpleService {
         } catch (e: DateTimeParseException) {
             rawFoodIntakeStringList.map {
                 FoodIntake(
-                    foodInfoId = it[10],
-                    dataUuid = it[11],
-                    name = it[13],
-                    mealTimeType = MealTimeType.fromCode(it[3]),
+                    foodInfoId = it[16],
+                    dataUuid = it[15],
+                    name = it[8],
+                    mealTimeType = MealTimeType.fromCode(it[7]),
                     amount = it[2].toDouble(),
-                    comment = it[6],
-                    calorie = it[7].toDouble(),
-                    eatenDate = LocalDate.parse(it[5], DateTimeFormatter.ofPattern("yyyy. MM. dd. hh:mm:ss", Locale.KOREA))
+                    comment = it[13],
+                    calorie = it[10].toDouble(),
+                    eatenDate = LocalDate.parse(it[1], DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss.SSS", Locale.KOREA))
                 )
             }
         }
 
 
-
-        val foodInfoList = rawFoodInfoStringList.map {
-            FoodInfo(
-                dataUuid = it[15],
-                name = it[14],
-                cholesterol = it[0].toDouble(),
-                servingDescription = it[1],
-                potassium = it[2].toDouble(),
-                sodium = it[3].toDouble(),
-                transFat = it[7].toDouble(),
-                carbohydrate = it[8].toDouble(),
-                metricServingAmount = it[11].toDoubleOrNull(),
-                calcium = it[12].toDouble(),
-                monosaturatedFat = it[13].toDouble(),
-                sugar = it[16].toDouble(),
-                saturatedFat = it[17].toDouble(),
-                unitCountPerCalorie = it[18].toDoubleOrNull(),
-                vitaminA = it[19].toDouble(),
-                metricServingUnit = it[20],
-                vitaminC = it[21].toDouble(),
-                calorie = it[22].toDouble(),
-                protein = it[23].toDouble(),
-                totalFat = it[24].toDouble(),
-                dietaryFiber = it[25].toDouble(),
-                iron = it[26].toDouble(),
-                polysaturatedFat = it[29].toDouble(),
-                defaultNumberOfServingUnit = it[30].toIntOrNull(),
-                description = it[31],
-            )
+        val foodInfoList = try {
+            rawFoodInfoStringList.map {
+                FoodInfo(
+                    dataUuid = it[15],
+                    name = it[14],
+                    cholesterol = it[0].toDouble(),
+                    servingDescription = it[1],
+                    potassium = it[2].toDouble(),
+                    sodium = it[3].toDouble(),
+                    transFat = it[7].toDouble(),
+                    carbohydrate = it[8].toDouble(),
+                    metricServingAmount = it[11].toDoubleOrNull(),
+                    calcium = it[12].toDouble(),
+                    monosaturatedFat = it[13].toDouble(),
+                    sugar = it[16].toDouble(),
+                    saturatedFat = it[17].toDouble(),
+                    unitCountPerCalorie = it[18].toDoubleOrNull(),
+                    vitaminA = it[19].toDouble(),
+                    metricServingUnit = it[20],
+                    vitaminC = it[21].toDouble(),
+                    calorie = it[22].toDouble(),
+                    protein = it[23].toDouble(),
+                    totalFat = it[24].toDouble(),
+                    dietaryFiber = it[25].toDouble(),
+                    iron = it[26].toDouble(),
+                    polysaturatedFat = it[29].toDouble(),
+                    defaultNumberOfServingUnit = it[30].toIntOrNull(),
+                    description = it[31],
+                )
+            }
+        } catch (e: NumberFormatException) {
+            val rawFoodInfoStringList = rawFoodInfoCsvStringData
+                .split("\n")
+                .map {
+                    it.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)".toRegex())
+                }
+                .drop(2)
+                .filter { it.size > 1 }
+            rawFoodInfoStringList.map {
+                FoodInfo(
+                    dataUuid = it[32],
+                    name = it[18],
+                    cholesterol = it[4].toDoubleOrNull() ?: 0.0,
+                    servingDescription = it[23],
+                    potassium = it[0].toDoubleOrNull() ?: 0.0,
+                    sodium = it[9].toDoubleOrNull() ?: 0.0,
+                    transFat = it[28].toDoubleOrNull() ?: 0.0,
+                    carbohydrate = it[30].toDoubleOrNull() ?: 0.0,
+                    metricServingAmount = it[8].toDoubleOrNull(),
+                    calcium = it[21].toDoubleOrNull() ?: 0.0,
+                    monosaturatedFat = it[14].toDoubleOrNull() ?: 0.0,
+                    sugar = it[19].toDoubleOrNull() ?: 0.0,
+                    saturatedFat = it[27].toDoubleOrNull() ?: 0.0,
+                    unitCountPerCalorie = it[31].toDoubleOrNull(),
+                    vitaminA = it[1].toDoubleOrNull() ?: 0.0,
+                    metricServingUnit = it[26],
+                    vitaminC = it[2].toDoubleOrNull() ?: 0.0,
+                    calorie = it[22].toDoubleOrNull() ?: 0.0,
+                    protein = it[15].toDoubleOrNull() ?: 0.0,
+                    totalFat = it[11].toDoubleOrNull() ?: 0.0,
+                    dietaryFiber = it[10].toDoubleOrNull() ?: 0.0,
+                    iron = it[17].toDoubleOrNull() ?: 0.0,
+                    polysaturatedFat = it[16].toDoubleOrNull() ?: 0.0,
+                    defaultNumberOfServingUnit = it[33].toIntOrNull(),
+                    description = it[5],
+                )
+            }
         }
+
 
         val foodIntakeToFoodInfoList = foodIntakeList.map { foodIntake ->
             foodIntake to foodInfoList.single { it.dataUuid == foodIntake.foodInfoId } // may use hash
@@ -138,6 +178,7 @@ object SimpleService {
             """.trimIndent()
             stringInputForOpenAI
         }
+        println(stringOutputPerDate.last())
         return stringOutputPerDate.last() // TODO: Last?
     }
 
@@ -185,9 +226,9 @@ object SimpleService {
             } else {
                 "ok"
             },
-            calcium = if (foodIntakeToFoodInfoList.sumOf { it.second.calcium } > 1200) {
+            calcium = if (foodIntakeToFoodInfoList.sumOf { it.second.calcium } > 150) {
                 "high"
-            } else if (foodIntakeToFoodInfoList.sumOf { it.second.calcium } < 800) {
+            } else if (foodIntakeToFoodInfoList.sumOf { it.second.calcium } < 80) {
                 "low"
             } else {
                 "ok"
@@ -213,14 +254,14 @@ object SimpleService {
             } else {
                 "ok"
             },
-            vitaminA = if (foodIntakeToFoodInfoList.sumOf { it.second.vitaminA } > 900) {
+            vitaminA = if (foodIntakeToFoodInfoList.sumOf { it.second.vitaminA } > 150) {
                 "high"
-            } else if (foodIntakeToFoodInfoList.sumOf { it.second.vitaminA } < 600) {
+            } else if (foodIntakeToFoodInfoList.sumOf { it.second.vitaminA } < 80) {
                 "low"
             } else {
                 "ok"
             },
-            vitaminC = if (foodIntakeToFoodInfoList.sumOf { it.second.vitaminC } > 200) {
+            vitaminC = if (foodIntakeToFoodInfoList.sumOf { it.second.vitaminC } > 150) {
                 "high"
             } else if (foodIntakeToFoodInfoList.sumOf { it.second.vitaminC } < 80) {
                 "low"
@@ -248,9 +289,9 @@ object SimpleService {
             } else {
                 "ok"
             },
-            iron = if (foodIntakeToFoodInfoList.sumOf { it.second.iron } > 20) {
+            iron = if (foodIntakeToFoodInfoList.sumOf { it.second.iron } > 150) {
                 "high"
-            } else if (foodIntakeToFoodInfoList.sumOf { it.second.iron } < 5) {
+            } else if (foodIntakeToFoodInfoList.sumOf { it.second.iron } < 80) {
                 "low"
             } else {
                 "ok"
